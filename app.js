64c4429f024eb7aa3e2a55fb1e22b127abf1f2ef
Masoud <masoud.zeinalabedini@gmail.com>
2023-12-13 11:25:23 +0330

    Send back response to browser

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("url:");
-  console.log(req.url);
-  console.log("**--++++--**");
-  console.log("method:");
-  console.log(req.method);
-  console.log("**--++++--**");
-  console.log("headers:");
-  console.log(req.headers);
+  console.log(req.url, req.method, req.headers);
   //   process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First page</title></head>");
+  res.write("<body><h1>This is my first page rendered on the server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(8800);
