e0839297b59e07e45e442d02b910e62ec8b8645c
Masoud <masoud.zeinalabedini@gmail.com>
2023-12-13 11:36:58 +0330

    detect the url and send a form to user with post methon form

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit();
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First page</title></head>");
-  res.write("<body><h1>This is my first page rendered on the server!</h1></body>");
+  res.write(
+    "<body><h1>This is my first page rendered on the server!</h1></body>"
+  );
   res.write("</html>");
   res.end();
 });
