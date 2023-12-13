c33ac317737c747305557a95509ad0a5794d9f2c
Masoud <masoud.zeinalabedini@gmail.com>
2023-12-13 11:09:16 +0330

    Log the request

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require("http");
 
-
-
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   console.log(req);
+//   process.exit();
 });
+
+server.listen(8800);
+
+console.log("you can access the server on:");
+console.log("localhost:8800");
