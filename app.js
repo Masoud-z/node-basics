26903c6189e3750faa0fab8d02fd10fa90414d6f
Masoud <masoud.zeinalabedini@gmail.com>
2023-12-18 13:28:25 +0330

    Get message from user and store it in message.txt file

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      console.log(chunk.length);
+      body.push(chunk);
+      console.log(body.toString());
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
